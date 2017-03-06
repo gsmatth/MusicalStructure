b9a12ed545e3b2779841f9a40c8525da0cd231c5
Doug Popadince <gsmatth@icloud.com>
2017-03-06 12:28:38 -0700

    added initial layout, methods and intents
    *added 6 activities
    *added 6 layouts
    *added basic styles
    *need to refactor and improve on all items above
    *rudimentary MVP exists
    modified:   app/src/main/AndroidManifest.xml
    modified:   app/src/main/java/com/example/android/musicalstructure/MainActivity.java
    new file:   app/src/main/java/com/example/android/musicalstructure/NowPlayingActivity.java
    new file:   app/src/main/java/com/example/android/musicalstructure/PreferencesActivity.java
    new file:   app/src/main/java/com/example/android/musicalstructure/RecommendationsActivity.java
    new file:   app/src/main/java/com/example/android/musicalstructure/TicketsActivity.java
    new file:   app/src/main/java/com/example/android/musicalstructure/TrendingActivity.java
    modified:   app/src/main/res/layout/activity_main.xml
    new file:   app/src/main/res/layout/activity_now_playing.xml
    new file:   app/src/main/res/layout/activity_preferences.xml
    new file:   app/src/main/res/layout/activity_recommedations.xml
    new file:   app/src/main/res/layout/activity_tickets.xml
    new file:   app/src/main/res/layout/activity_trending.xml
    modified:   app/src/main/res/values/colors.xml
    new file:   app/src/main/res/values/dimens.xml/dimens.xml
    modified:   app/src/main/res/values/strings.xml
    modified:   app/src/main/res/values/styles.xml

---
+++ app/src/main/java/com/example/android/musicalstructure/PreferencesActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.musicalstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.android.musicalstructure.R;
+
+/**
+ * Created by djp on 3/6/17.
+ */
+
+public class PreferencesActivity extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
---
+++ app/src/main/java/com/example/android/musicalstructure/TrendingActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.musicalstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by djp on 3/6/17.
+ */
+
+public class TrendingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending);
+    }
+}
